453090e00532ee7a707efb222b2fa8cc6b1f9353
Kasekopf <jeffreydudek@gmail.com>
2022-07-31 14:59:07 -0700

    Print all runs on the page

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,34 @@
-import { myMp, print } from "kolmafia";
+import { print, visitUrl } from "kolmafia";
 
-export function checkMP(): string {
-  if (myMp() < 200) {
-    return "Your MP is less than 200.";
-  } else {
-    return "Your MP is greater than or equal to 200.";
-  }
-}
-
+type Run = {
+  id: number;
+  player: string;
+  class: string;
+  path: string;
+  days: number;
+  turns: number;
+};
 export function main(): void {
-  print(checkMP());
+  const page = visitUrl("museum.php?floor=1&place=leaderboards&whichboard=999&showhist=500");
+  const runs: Run[] = [];
+
+  const run_regex = new RegExp(
+    /<tr><td>.*?<\/td><td><a href=showplayer.php\?who=(\d+) class=nounder><b>(.*?)<\/b><\/a>&nbsp;<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td align=right>(.*?)<\/td><\/tr>/g
+  );
+  let match;
+  do {
+    match = run_regex.exec(page);
+    if (match) {
+      runs.push({
+        id: parseInt(match[1]),
+        player: match[2],
+        class: match[3],
+        path: match[4],
+        days: parseInt(match[5].replace(",", "")),
+        turns: parseInt(match[6].replace(",", "")),
+      });
+    }
+  } while (match);
+
+  runs.forEach((run) => print(JSON.stringify(run)));
 }
